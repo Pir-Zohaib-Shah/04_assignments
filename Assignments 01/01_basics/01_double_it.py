c4c1eb02739b7774ec698a5e528e4f2c940ed95c
Zohaib Shah <Zohaibshahak47@gmail.com>
2025-04-25 04:23:21 +0500

    assignments added

---
+++ Assignments 01/01_basics/01_double_it.py
@@ -0,0 +1,15 @@
+def main():
+
+    print("Welcome to the double it program!")
+
+    user_input = int(input("Enter a number: "))
+    curr_value = user_input
+
+    while curr_value < 100:
+
+        curr_value = curr_value * 2
+
+        print(curr_value)
+        
+if __name__ == "__main__":
+    main()  
