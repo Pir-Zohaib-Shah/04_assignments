61bcf3392bb3eef9cd2709bc49d143365fc96b71
Zohaib Shah <Zohaibshahak47@gmail.com>
2025-04-25 04:43:42 +0500

    assignment added

---
+++ Assignments 1 to 6/hangman/hangman.py
@@ -0,0 +1,47 @@
+import random
+import string
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words).upper()
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    lives = 7
+
+    while len(word_letters) > 0 and lives > 0:
+        print("you have " + str(lives) + " lives left you have used these letters: ", ' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('\nCurrent word: ', ' '.join(word_list))
+
+        user_letter = input('Type a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+             used_letters.add(user_letter)
+             if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+             else:
+                lives = lives - 1
+                print("letter is not in word\n")
+
+        elif user_letter in used_letters:
+               print('You have already used that character. Please try again.')
+
+        else:
+             print('Invalid character. Please try again.') 
+
+    if lives == 0:
+        print(f"Game Over! the word was {word}!!")
+    else:
+        print(f"You Won & Guessed the word {word} Correctly!")
+
+hangman()
